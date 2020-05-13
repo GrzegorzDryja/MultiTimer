7c094fbd7585888695f765f869701c8beecd801a
Grzegorz Dryja <grz8d3@gmail.com>
2020-05-13 14:36:47 +0200

    setTimeout
    Works with setTimeout method, done it with MultiTimer class and functions as a variable with this keyword from it.

--- js/timer.js
+++ js/timer.js
@@ -1,34 +1,58 @@
+window.onload = function()
+{
+    var start = document.querySelector("#startButton");
+    var pause = document.querySelector("#pauseButton");    
+    var seconds = document.querySelector("#seconds");
+    var reset = document.querySelector("#resetButton");
 
-var startButton = document.querySelector("#startButton");
-var pauseButton = document.querySelector("#pauseButton");
-var resetButton = document.querySelector("#resetButton");
-var pauseBool = false;
+    var timer = new MultiTimer(seconds);
 
-startButton.addEventListener("click", start);
-pauseButton.addEventListener("click", pause);
-resetButton.addEventListener("click", reset);
-
-function start(){
-    var startValue = document.querySelector("#seconds").value;
-    pauseBool = false
-    if(startValue > 0){
-        var timer = setInterval(function()
-                {   
-                    document.querySelector("#seconds").value = --startValue;                 
-                    if (pauseBool || startValue <= 0)
-                    {
-                        clearInterval(timer);
-                    }
-                    
-                }, 1000);
-
+    start.onclick = function()
+    {
+        var startValue = document.querySelector("#seconds").value; 
+        timer.start(startValue);
+    };
+    pause.onclick = function()
+    {
+        timer.pause();
+    };
+    reset.onclick = function()
+    {
+        timer.reset();
     };
 };
 
-function pause(){    
-    pauseBool = true;
-};
+function MultiTimer(seconds)
+{
+    this.seconds = seconds;
+    this.startValue;
+    this.timeOutRef;
 
-function reset(){    
-    document.querySelector("#seconds").value = "00";
-};
+    this.start = function(startValue)
+                                {
+                                    this.startValue = startValue;
+                                    if (this.timeOutRef)
+                                        this.pause();										        
+                                    this.startStoper();
+                                };
+    this.startStoper = function()
+                                {
+                                    if (this.startValue < 0)
+                                    return;                                    
+                                    this.seconds.value = this.startValue--;                                    
+                                    var self = this;                                    
+                                    this.timeOutRef = setTimeout(function()
+                                    {
+                                        self.startStoper();
+                                    }, 1000);
+                                };
+    this.pause = function()
+							    {
+							        clearTimeout(this.timeOutRef);
+							    };
+    this.reset = function()
+							    {
+                                    document.querySelector("#seconds").value = "00";
+                                    clearTimeout(this.timeOutRef);
+							    };
+}
