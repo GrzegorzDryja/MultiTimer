7a97f6c91391d2047886f868a51c17a9697fcde1
Grzegorz Dryja <grz8d3@gmail.com>
2020-05-13 13:53:34 +0200

    Test
    Test

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 window.onload = function()
 {
     var start = document.querySelector("#startButton"),
@@ -91,3 +92,39 @@ function Timer(minutes, seconds)
             clearTimeout(this.timeOutRef);
         };
 }
+=======
+
+var startButton = document.querySelector("#startButton");
+var pauseButton = document.querySelector("#pauseButton");
+var resetButton = document.querySelector("#resetButton");
+var pauseBool = false;
+
+startButton.addEventListener("click", start);
+pauseButton.addEventListener("click", pause);
+resetButton.addEventListener("click", reset);
+
+function start(){
+    var startValue = document.querySelector("#seconds").value;
+    pauseBool = false
+    if(startValue > 0){
+        var timer = setInterval(function()
+                {   
+                    document.querySelector("#seconds").value = --startValue;                 
+                    if (pauseBool || startValue <= 0)
+                    {
+                        clearInterval(timer);
+                    }
+                    
+                }, 1000);
+
+    };
+};
+
+function pause(){    
+    pauseBool = true;
+};
+
+function reset(){    
+    document.querySelector("#seconds").value = "00";
+};
+>>>>>>> 0c9dc88... setInterval
