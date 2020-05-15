7f0fd3e31007cbc521b947404936c56e7aa41947
Grzegorz Dryja <grz8d3@gmail.com>
2020-05-15 13:38:26 +0200

    MultiTimer
    MultiTimer works for multiplay div, only for consol.
    Issue - input value  do not change.

--- js/timer.js
+++ js/timer.js
@@ -1,122 +1,13 @@
 window.onload = function()
 {
-    createTimerDiv(); 
-    
-                                    var start = document.querySelector("#startButton"),
-                                        pause = document.querySelector("#pauseButton"),
-                                        minutes = document.querySelector("#minutes"),    
-                                        seconds = document.querySelector("#seconds"),
-                                        reset = document.querySelector("#resetButton"),
-                                        name = document.querySelector("#nameInput"),
-                                        add = document.querySelector("#addButton"),
-                                        remove = document.querySelector("#removeButton"),
-                                        timers = [];
-                                    
-    var timer = new Timer(minutes, seconds);
+    var timer2 = new CreateTimerDiv(2);
+    timer2.buttonsSupport(2);
 
-                                    name.addEventListener("click", function()
-                                        {   
-                                            var oldName = document.querySelector("#nameInput").innerHTML;
-                                            name.value = ""; //Remember that cleans data           
-                                        });
-
-                                    name.addEventListener("dblclick", function()
-                                        {
-                                            name.disabled=false;
-                                        });
-
-                                    name.addEventListener("keypress", function(e)
-                                        {
-                                            if(e.key === 'Enter')
-                                            {                                                
-                                            var name = document.querySelector("#nameInput").value;
-                                            }
-                                        });
-
-                                    start.addEventListener("click", function()
-                                        {
-                                            var startMinutes = document.querySelector("#minutes").value,
-                                                startSeconds = document.querySelector("#seconds").value;
-
-    timer.start(startMinutes, startSeconds);
-                                        });
-
-                                    pause.addEventListener("click", function()
-                                        {
-    timer.pause();
-                                        });
-
-                                    reset.addEventListener("click", function()
-                                        {
-    timer.reset();
-                                        });
-                                    add.addEventListener("click", createTimer);
-                                    remove.addEventListener("click", function()
-                                        {
-                                            remove();
-                                        });   
-                                };
-
-function Timer(minutes, seconds)
-{
-    this.minutes = minutes;
-    this.seconds = seconds;
-    this.startMinutes;
-    this.startSeconds;
-    this.timeOutRef;
-
-    this.start = function(startMinutes, startSeconds)
-        {
-            this.startMinutes = startMinutes;
-            this.startSeconds = startSeconds;
-            if (this.timeOutRef)
-                this.pause();										        
-            this.startTimer();
-        };
-
-    this.startTimer = function()
-        {   if(this.startSeconds <= 0 && this.startMinutes <= 0)
-            return;
-
-            if(this.startSeconds == 0 && --this.startMinutes >= 0)
-            this.startSeconds = 60;
-            this.countSeconds();
-            this.countMinutes();
-        };
-
-     this.countMinutes = function()
-        {   
-            this.minutes.value = this.startMinutes;                                    
-        };
-
-    this.countSeconds = function()
-        {  
-            this.seconds.value = --this.startSeconds; 
-            var self = this;                                    
-            this.timeOutRef = setTimeout(function()
-            {
-                self.startTimer();
-            }, 50);            // Note: 1000!
-        };         
-
-    this.pause = function()
-        {
-            clearTimeout(this.timeOutRef);
-        };
-        
-    this.reset = function()
-        {
-            document.querySelector("#minutes").value = "00";
-            document.querySelector("#seconds").value = "00";
-            clearTimeout(this.timeOutRef);
-        };
+    var timer3 = new CreateTimerDiv(3);
+    timer3.buttonsSupport(3);
 };
 
-function add(){
-    createTimer();
-}
-
-function createTimerDiv(){    
+function CreateTimerDiv(id){    
 
     function div(){
         return document.createElement("div");
@@ -135,7 +26,7 @@ function createTimerDiv(){
     var inputName = input();
         inputName.type="text";
         inputName.className="name";
-        inputName.id="nameInput";
+        inputName.id="nameInput"+id;
         inputName.defaultValue="Name it here...";
         inputName.maxLength="10";
 
@@ -146,7 +37,7 @@ function createTimerDiv(){
     var inputMinutes = input();
         inputMinutes.type="text";
         inputMinutes.className="name";
-        inputMinutes.id="minutes";
+        inputMinutes.id="minutes"+id;
         inputMinutes.title="Minutes";
         inputMinutes.maxLength="2";
         inputMinutes.defaultValue="00";
@@ -158,7 +49,7 @@ function createTimerDiv(){
     var inputSeconds = input();
         inputSeconds.type="text";
         inputSeconds.className="name";
-        inputSeconds.id="seconds";
+        inputSeconds.id="seconds"+id;
         inputSeconds.title="Seconds";
         inputSeconds.maxLength="2";
         inputSeconds.defaultValue ="00";
@@ -167,53 +58,53 @@ function createTimerDiv(){
 
     var startButtonDiv = div();
         startButtonDiv.className="main button";
-        startButtonDiv.id="startButtonDiv";
+        startButtonDiv.id="startButtonDiv"+id;
         startButtonDiv.title="Start";
 
     var startButton = input();
         startButton.type="button";
         startButton.value="Start";
-        startButton.id="startButton";
+        startButton.id="startButton"+id;
 
     var pauseButtonDiv = div();
         pauseButtonDiv.className="main button";
-        pauseButtonDiv.id="pauseButtonDiv";
+        pauseButtonDiv.id="pauseButtonDiv"+id;
         pauseButtonDiv.title="Pause";
 
     var pauseButton = input();
         pauseButton.type="button";
         pauseButton.value="Stop";
-        pauseButton.id="pauseButton";
+        pauseButton.id="pauseButton"+id;
 
     var resetButtonDiv = div();
         resetButtonDiv.className="main button";
-        resetButtonDiv.id="resetButton";
+        resetButtonDiv.id="resetButton"+id;
         resetButtonDiv.title="Reset";
 
     var resetButton = input();
         resetButton.type="button";
         resetButton.value="Reset";
-        resetButton.id="resetButton";
+        resetButton.id="resetButton"+id;
 
     var addButtonDiv = div()
         addButtonDiv.className="main button";
-        addButtonDiv.id="addButton";
+        addButtonDiv.id="addButton"+id;
         addButtonDiv.title="Add";
 
     var addButton = input();
         addButton.type="button";
         addButton.value="Add";
-        addButton.id="addButton";
+        addButton.id="addButton"+id;
             
     var removeButtonDiv = div()
         removeButtonDiv.className="main button";
-        removeButtonDiv.id="removeButton";
+        removeButtonDiv.id="removeButton"+id;
         removeButtonDiv.title="Remove";
 
     var removeButton = input();
         removeButton.type="button";
         removeButton.value="Remove";
-        removeButton.id="removeButton";
+        removeButton.id="removeButton"+id;
 
     document.body.appendChild(timerDiv);
     timerDiv.appendChild(nameDiv);
@@ -234,3 +125,107 @@ function createTimerDiv(){
     addButtonDiv.appendChild(addButton);
     removeButtonDiv.appendChild(removeButton);
 }
+
+CreateTimerDiv.prototype.buttonsSupport = function(id){
+    this.name = document.querySelector("#nameInput"+id);
+    this.start = document.querySelector("#startButton"+id);
+    this.pause = document.querySelector("#pauseButton"+id);
+    this.minutes = document.querySelector("#minutes"+id);  
+    this.seconds = document.querySelector("#seconds"+id);
+    this.reset = document.querySelector("#resetButton"+id);
+    this.name = document.querySelector("#nameInput"+id);
+    this.add = document.querySelector("#addButton"+id);
+    this.remove = document.querySelector("#removeButton"+id);
+
+    this.minutesCount;
+    this.secondsCount;
+    this.timeOutRef;
+
+    function startCount(min, sec)
+        {               
+            this.minutesCount = min;
+            this.secondsCount = sec; 
+            
+            if (this.timeOutRef)
+                pauseCount();	
+            if(this.secondsCount <= 0 && this.minutesCount <= 0)
+            return;
+
+            if(this.secondsCount == 0 && this.minutesCount >= 0)
+                {
+                    this.secondsCount = 60;
+                    --this.minutesCount                                                                             
+                };                     
+            countSeconds(); 
+            console.log(this.minutesCount + ":" + this.secondsCount);                      
+        };       
+
+    function countSeconds()
+        {  
+            --this.secondsCount;
+            this.timeOutRef = setTimeout(function()
+                {
+                    startCount(this.minutesCount, this.secondsCount);
+                }, 50);                                                              // Note: 1000!
+        };
+
+    function pauseCount()
+        {
+            clearTimeout(this.timeOutRef);
+        };
+        
+    function resetCount()
+        {
+            document.querySelector("#minutes"+id).value = "00";
+            document.querySelector("#seconds"+id).value = "00";
+            clearTimeout(this.timeOutRef);
+        };
+
+    this.name.addEventListener("click", function()
+        {   
+            this.oldName = document.querySelector("#nameInput"+id).innerHTML;
+            name.value = ""; //Remember that cleans data           
+        });
+
+    this.name.addEventListener("dblclick", function()
+        {
+            name.disabled=false;
+        });
+
+    this.name.addEventListener("keypress", function(e)
+        {
+            if(e.key === 'Enter')
+            {                                                
+            this.name = document.querySelector("#nameInput"+id).value;
+            console.log(this.name)
+            }
+        });
+
+    this.start.addEventListener("click", function()
+        {
+            this.minutesValue = document.querySelector("#minutes"+id).value;
+            this.secondsValue = document.querySelector("#seconds"+id).value;
+            startCount(this.minutesValue, this.secondsValue);
+        });
+
+    this.pause.addEventListener("click", function()
+        {
+            console.log("pause"+id);
+            pauseCount();
+        });
+
+    this.reset.addEventListener("click", function()
+        {
+            console.log("reset"+id);
+            resetCount();
+        });
+    this.add.addEventListener("click", function()
+        {
+            console.log("add"+id);
+        });
+
+    this.remove.addEventListener("click", function()
+        {
+            console.log("remove"+id);
+        });   
+};
